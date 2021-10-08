b6e92ca1cd9d84f14569284d3c8023ad70ed0bf6
Juan Pablo Toledo <jpbernal34@misena.edu.co>
2021-10-08 12:50:19 -0500

    se añadieron crear, actualizar, borrar

--- src/main/java/com/misiontic/webfavorites/controller/UserController.java
+++ src/main/java/com/misiontic/webfavorites/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.misiontic.webfavorites.controller;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.misiontic.webfavorites.dtos.UserDTO;
@@ -35,7 +39,51 @@ public class UserController {
 		
 		return new ResponseEntity<List<UserDTO>>(userDto, HttpStatus.OK);
 		
-
+	}
+	
+	@GetMapping(value = "/user/{userId}")
+	public ResponseEntity<UserDTO> findById(@PathVariable("userId") Long userId){
+		User user = userService.fingById(userId);
 		
+		UserDTO userDto = UserDTO.builder()
+				.id(user.getId())
+				.name(user.getName())
+				.password(user.getPassword())
+				.email(user.getEmail())
+				.build();
+	
+		return new ResponseEntity<UserDTO>(userDto, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/user/{userId}")
+	public ResponseEntity<Void> delete(@PathVariable("userId") Long userId){
+		userService.delete(userId);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/user")
+	public ResponseEntity<UserDTO> create(@RequestBody User user){
+		User newUser = userService.save(user);
+		
+		UserDTO userDto = UserDTO.builder()
+				.id(user.getId())
+				.name(user.getName())
+				.password(user.getPassword())
+				.email(user.getEmail())
+				.build();
+		return new ResponseEntity<UserDTO>(userDto, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(value = "/user")
+	public ResponseEntity<UserDTO> update(@RequestBody User user){
+		User newUser = userService.save(user);
+		
+		UserDTO userDto = UserDTO.builder()
+				.id(user.getId())
+				.name(user.getName())
+				.password(user.getPassword())
+				.email(user.getEmail())
+				.build();
+		return new ResponseEntity<UserDTO>(userDto, HttpStatus.CREATED);
 	}
 }
--- src/main/java/com/misiontic/webfavorites/services/UserService.java
+++ src/main/java/com/misiontic/webfavorites/services/UserService.java
@@ -2,21 +2,56 @@ package com.misiontic.webfavorites.services;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.misiontic.webfavorites.entity.User;
 import com.misiontic.webfavorites.repository.UserRepository;
+import com.misiontic.webfavorites.validators.UserValidator;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
-	
+
 	public List<User> findAll() {
-		List<User> users= userRepo.findAll();
+		List<User> users = userRepo.findAll();
 		return users;
+	}
+
+	public User fingById(Long userId) {
+		User user = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("El usuario no existe"));
+		return user;
+	}
+
+	@Transactional
+	public void delete(Long userId) {
+		User user = userRepo.findById(userId).orElseThrow(() -> new RuntimeException("El usuario no existe"));
+
+		userRepo.delete(user);
+	}
+
+	@Transactional
+	public User save(User user) {
+		UserValidator.save(user);
+
+		if (user.getId() == null) {
+			User newUser = userRepo.save(user);
+			return newUser;
+		}
+
+		User updateUser = userRepo.findById(user.getId())
+				.orElseThrow(() -> new RuntimeException("El usuario no existe."));
+		
+		updateUser.setName(user.getName());
+		updateUser.setPassword(user.getPassword());
+		updateUser.setEmail(user.getEmail());
+		
+		userRepo.save(updateUser);
+		
+		return updateUser;
 	}
 }
